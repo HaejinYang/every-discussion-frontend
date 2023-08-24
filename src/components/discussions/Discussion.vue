ce0fadbfd3c5ad08af1560d8cf3460eca8509293
haejin <haejin.yang91@gmail.com>
2023-08-24 14:54:51 +0900

    feat:  작업3. 토픽에 속한 찬반 의견을 위한 레이아웃 (#5)

---
+++ src/components/discussions/Discussion.vue
@@ -0,0 +1,85 @@
+<template>
+  <div :class="[$style.container, agreeingType === 'agree' ? $style.agree : $style.disagree]">
+    <div :class="$style.title">
+      <span>{{ agreeingType === 'agree' ? '찬성' : '반대' }}</span>
+    </div>
+    <div :class="$style.opinion">
+      <p>
+        Lorem ipsum dolor sit amet. Sed accusantium fugiat est dignissimos commodi in laborum natus
+        qui quisquam sint aut quia provident eos quis optio.
+      </p>
+      <div>추천1 비추천2</div>
+    </div>
+    <div :class="$style.opinion">
+      <p>
+        Lorem ipsum dolor sit amet. Sed accusantium fugiat est dignissimos commodi in laborum natus
+        qui quisquam sint aut quia provident eos quis optio.
+      </p>
+      <div>추천1 비추천2</div>
+    </div>
+    <div :class="$style.opinion">
+      <p>
+        Lorem ipsum dolor sit amet. Sed accusantium fugiat est dignissimos commodi in laborum natus
+        qui quisquam sint aut quia provident eos quis optio.
+      </p>
+      <div>추천1 비추천2</div>
+    </div>
+    <div :class="$style.opinion">
+      <p>
+        Lorem ipsum dolor sit amet. Sed accusantium fugiat est dignissimos commodi in laborum natus
+        qui quisquam sint aut quia provident eos quis optio.
+      </p>
+      <div>추천1 비추천2</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Discussion',
+  props: {
+    agreeingType: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style module lang="scss">
+.container {
+  background-color: #eee;
+  max-width: 300px;
+
+  .title {
+    padding: 1rem 1rem 0 1rem;
+    text-align: center;
+  }
+
+  .opinion {
+    margin: 0.5rem;
+    border: $border-weak-line;
+
+    > p,
+    div {
+      padding: 0.5rem;
+      margin: 0.5rem;
+      border-bottom: $border-weak-line;
+    }
+
+    div {
+      text-align: right;
+    }
+  }
+}
+
+.agree {
+  background-color: deepskyblue;
+}
+
+.disagree {
+  background-color: palevioletred;
+}
+</style>
--- src/views/DiscussionView.vue
+++ src/views/DiscussionView.vue
@@ -1,19 +1,76 @@
 <template>
-  <div>Discussion {{ topicId }}</div>
+  <div :class="$style.container">
+    <div :class="$style.title">
+      <h1>토픽주제입니다</h1>
+    </div>
+    <div :class="$style.opinions">
+      <div :class="$style.agree">
+        <Discussion agreeingType="agree" />
+        <div :class="$style['submit-agree']">
+          <button>의견제시</button>
+        </div>
+      </div>
+
+      <div :class="$style.disagree">
+        <Discussion agreeingType="disagree" />
+        <div :class="$style['submit-disagree']">
+          <button>의견제시</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Discussion from '@/components/discussions/Discussion.vue';
 
 export default defineComponent({
   name: 'DiscussionView',
+  components: { Discussion },
   props: {
     topicId: {
       type: Number,
       required: true
     }
+  },
+  mounted() {
+    // get topic id
   }
 });
 </script>
 
-<style module lang="scss"></style>
+<style module lang="scss">
+.container {
+  background-color: #eee;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    padding: 1rem;
+  }
+
+  .opinions {
+    display: flex;
+
+    .agree {
+      padding: 1rem;
+      border: $border-normal-line;
+    }
+
+    .disagree {
+      padding: 1rem;
+      border: $border-normal-line;
+    }
+
+    .submit-agree,
+    .submit-disagree {
+      padding: 1rem;
+      margin: 1rem;
+      text-align: center;
+    }
+  }
+}
+</style>
