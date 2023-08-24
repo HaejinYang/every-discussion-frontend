f9be10e0f850bf3ef9d77dbc8f73526dd435b320
haejin <haejin.yang91@gmail.com>
2023-08-24 14:16:47 +0900

    feat: 작업1. 라우트 등록하고 토픽ID를 프롭스로 받음 (#5)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,22 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import DiscussionView from '@/views/DiscussionView.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView
-        }
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/discussion/:topicId',
+      name: 'discussion',
+      component: DiscussionView,
+      props: true
+    }
+  ]
+});
 
-export default router
+export default router;
---
+++ src/views/DiscussionView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Discussion {{ topicId }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'DiscussionView',
+  props: {
+    topicId: {
+      type: Number,
+      required: true
+    }
+  }
+});
+</script>
+
+<style module lang="scss"></style>
