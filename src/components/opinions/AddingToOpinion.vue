90a309c43d64071763c825cd74c5217f39d0c271
haejin.yang91 <haejin.yang91@gmail.com>
2023-12-07 21:32:52 +0900

    feat: 작업 3. AddingToOpinion 컴포넌트 컴포지션 API로 변경 (#45)

--- src/components/opinions/AddingToOpinion.vue
+++ src/components/opinions/AddingToOpinion.vue
@@ -11,90 +11,76 @@
   </ModalContainer>
 </template>
 
-<script lang="ts">
-import { defineComponent, type PropType } from 'vue';
-import OpinionWriterForm from '@/components/opinions/OpinionWriterForm.vue';
-import { getErrorMessage } from '@/util/error';
-import { UserOpinionService } from '@/services/opinions/UserOpinionService';
-import ModalContainer from '@/components/common/modals/ModalContainer.vue';
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import {UserOpinionService} from "@/services/opinions/UserOpinionService";
+import {getErrorMessage} from "@/util/error";
+import OpinionWriterForm from "@/components/opinions/OpinionWriterForm.vue";
+import ModalContainer from "@/components/common/modals/ModalContainer.vue";
 
-enum eProcess {
-  Init = 0,
-  Wait = 1,
-  Success = 2,
-  Fail = 3
-}
-
-export default defineComponent({
+defineOptions({
   name: 'AddingToOpinion',
-  emits: ['result', 'on-click-close'],
-  props: {
-    targetOpinionId: {
-      type: Number,
-      required: true
-    },
-    type: {
-      type: String as PropType<'agree' | 'disagree'>,
-      required: true
-    },
-    topicId: {
-      type: Number,
-      required: true
-    },
-    title: {
-      type: String,
-      required: true
-    }
-  },
-  components: { OpinionWriterForm, ModalContainer },
-  data() {
-    return {
-      processingStep: eProcess.Init as eProcess,
-      processingStepMsg: ['제출', '', '성공', '실패']
-    };
-  },
-  computed: {
-    isShowWait() {
-      return this.processingStep === eProcess.Wait;
-    }
-  },
-  methods: {
-    onClickPage() {
-      this.$emit('on-click-close');
-    },
-    onClickForm() {
-      if (this.processingStep === eProcess.Fail) {
-        this.processingStep = eProcess.Init;
-      }
-    },
-    async onSubmit(opinion: { title: string; body: string }) {
-      if (this.processingStep === eProcess.Wait || this.processingStep === eProcess.Success) {
-        return;
-      }
+})
 
-      const title = opinion.title;
-      const body = opinion.body;
+interface Props {
+  targetOpinionId: number;
+  type: 'agree' | 'disagree';
+  topicId: number;
+  title: string;
+}
+  enum eProcess {
+    Init = 0,
+    Wait = 1,
+    Success = 2,
+    Fail = 3
+  };
 
-      const userOpinionService = new UserOpinionService();
-      try {
-        this.processingStep = eProcess.Wait;
-        await userOpinionService.create({
-          topicId: this.topicId,
-          title: title,
-          content: body,
-          agreeingType: this.type,
-          addTo: this.targetOpinionId
-        });
-        this.processingStep = eProcess.Success;
-        this.$emit('result', 'success');
-      } catch (e) {
-        reportError(getErrorMessage(e));
-        this.processingStep = eProcess.Fail;
-        this.$emit('result', 'fail');
-      }
+  const props = defineProps<Props>();
+
+  const emit = defineEmits(['result', 'on-click-close']);
+
+  const processingStep = ref(eProcess.Init as eProcess);
+  const processingStepMsg = ref(['제출', '', '성공', '실패']);
+  const isShowWait = computed(() => {
+    return processingStep.value === eProcess.Wait;
+  });
+
+  function onClickPage() {
+    emit('on-click-close');
+  }
+
+  function onClickForm() {
+    if (processingStep.value === eProcess.Fail) {
+      processingStep.value = eProcess.Init;
     }
   }
-});
+
+  async function onSubmit(opinion: { title: string; body: string }) {
+    if (processingStep.value === eProcess.Wait || processingStep.value === eProcess.Success) {
+      return;
+    }
+
+    const title = opinion.title;
+    const body = opinion.body;
+
+    const userOpinionService = new UserOpinionService();
+    try {
+      processingStep.value = eProcess.Wait;
+      await userOpinionService.create({
+        topicId: props.topicId,
+        title: title,
+        content: body,
+        agreeingType: props.type,
+        addTo: props.targetOpinionId
+      });
+      processingStep.value = eProcess.Success;
+      emit('result', 'success');
+    } catch (e) {
+      reportError(getErrorMessage(e));
+      processingStep.value = eProcess.Fail;
+      emit('result', 'fail');
+    }
+  }
 </script>
 
 <style module lang="scss"></style>
